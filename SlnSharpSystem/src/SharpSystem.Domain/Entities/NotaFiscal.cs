7ecbb732bd6b62220da5937df7d8a18daddd0c40
Gustavo Schimite Soares <gustavoschimitesoares0211@gmail.com>
2023-03-11 14:43:18 -0300

    Entidades renomeadas conforme requisição XML

--- SlnSharpSystem/src/SharpSystem.Domain/Entities/NotaFiscal.cs
+++ SlnSharpSystem/src/SharpSystem.Domain/Entities/NotaFiscal.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace SharpSystem.Domain.Entities
 {
@@ -10,5 +11,14 @@ namespace SharpSystem.Domain.Entities
     {
         public int Id { get; set; }
         public string ValorTotal { get; set; }
+
+        [XmlElement("prestador")]
+        public Prestador Prestador { get; set; }
+
+        [XmlElement("tomador")]
+        public Tomador Tomador { get; set; }
+
+        [XmlElement("itens")]
+        public Itens Itens { get; set; }
     }
 }
--- SlnSharpSystem/src/SharpSystem.Domain/Entities/Tomador.cs
+++ SlnSharpSystem/src/SharpSystem.Domain/Entities/Tomador.cs
@@ -10,6 +10,7 @@ namespace SharpSystem.Domain.Entities
     {
         public int Id { get; set; }
         public string Tipo { get; set; }
+        public string CpfCnpj { get; set; }
         public string NomeRazaoSocial { get; set; }
         public string Logradouro { get; set; }
         public string Email { get; set; }
