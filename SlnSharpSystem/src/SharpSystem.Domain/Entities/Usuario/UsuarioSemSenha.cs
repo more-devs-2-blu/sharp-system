d0468891140e931586c15a35138ad1590c44fcdb
Pedro Henrique GOdri <pedrogodri@gmail.com>
2023-03-11 19:14:10 -0300

    feat: Entidades do Usuario

---
+++ SlnSharpSystem/src/SharpSystem.Domain/Entities/Usuario/UsuarioSemSenha.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpSystem.Domain.Entities.Usuario
+{
+    public class UsuarioSemSenha
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+    }
+}
