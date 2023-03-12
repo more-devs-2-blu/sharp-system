ead7012c05b1f6244992e037298b3be4f79ee06b
Pedro Henrique GOdri <pedrogodri@gmail.com>
2023-03-12 18:46:22 -0300

    feat: atualizando o WebServiceAPI

--- SlnSharpSystem/src/SharpSystem.Application/Services/WebServiceAPI.cs
+++ SlnSharpSystem/src/SharpSystem.Application/Services/WebServiceAPI.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using SharpSystem.Application.Interfaces;
+using SharpSystem.Domain.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace SharpSystem.Application.Services
 {
-    internal class WebServiceAPI
+    public class WebServiceAPI : IWebServiceAPI
     {
+        public string EmitirNotaFiscal(XmlSerializer xmlNotaFiscal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public XmlSerializer EmitirXMLSerializaer(NFESDTO nfse)
+        {
+            throw new NotImplementedException();
+        }
+
+        public XmlSerializer GetFile(NFESDTO nfse)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
