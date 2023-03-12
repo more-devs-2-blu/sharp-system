20e61238855c8341d1fc19149b760c339c38c9a2
Gustavo Schimite Soares <gustavoschimitesoares0211@gmail.com>
2023-03-12 13:31:39 -0300

    Atualizando branch

--- SlnSharpSystem/src/SharpSystem.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SharpSystem.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
