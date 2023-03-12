8e76c0782341849ace42f8df491fa2ba25b20dee
Pedro Henrique GOdri <pedrogodri@gmail.com>
2023-03-12 18:43:07 -0300

    feat: DTO, Repositories, Services da NFs e iniando o WebServices

--- SlnSharpSystem/src/SharpSystem.Domain/DTO/NFESDTO.cs
+++ SlnSharpSystem/src/SharpSystem.Domain/DTO/NFESDTO.cs
@@ -1,13 +1,22 @@
-﻿using System;
+﻿using SharpSystem.Domain.Entities.NF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace SharpSystem.Domain.DTO
 {
     public class NFESDTO
     {
-        public int MyProperty { get; set; }
+        [XmlElement("nf")]
+        public NotaFiscal NF { get; set; }
+        [XmlElement("prestador")]
+        public Prestador Prestador { get; set; }
+        [XmlElement("tomador")]
+        public Tomador Tomador { get; set; }
+        [XmlElement("itens")]
+        public List<Prestador> Itens { get; set; }
     }
 }
