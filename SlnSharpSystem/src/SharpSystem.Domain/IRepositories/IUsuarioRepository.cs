c1a19d73f629b67ccb75092fe0849c1808f9df66
Gustavo Schimite Soares <gustavoschimitesoares0211@gmail.com>
2023-03-12 13:33:47 -0300

    Atualizando branch

---
+++ SlnSharpSystem/src/SharpSystem.Domain/IRepositories/IUsuarioRepository.cs
@@ -0,0 +1,14 @@
+﻿using SharpSystem.Domain.Entities.Usuario;
+using SharpSystem.Domain.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpSystem.Domain.IRepositories
+{
+    public interface IUsuarioRepository : IBaseRepository<Usuario>
+    {
+    }
+}
---
+++ SlnSharpSystem/src/SharpSystem.Infra.Data/Context/SQLServerContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SharpSystem.Domain.Entities.Usuario;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpSystem.Infra.Data.Context
+{
+    public class SQLServerContext : DbContext   
+    {
+        public SQLServerContext(DbContextOptions<SQLServerContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>()
+            .HasData(
+                    new { Id = 1, Nome = "Pedro Godri", CFPCNPJ = "104.752.299-23", Email = "pedrogodri@gmail.com", Login = "PedroGodri", Senha = "12345" }
+                    );
+        }
+
+        #region DbSets<Tables>
+        public DbSet<Usuario> Usuarios { get; set; }
+        #endregion
+    }
+}
