c1703345e63d6c370500fb482b6bbf5e140f612d
Pedro Henrique GOdri <pedrogodri@gmail.com>
2023-03-13 16:56:39 -0300

    feat: Criando a Sessao do Usuario

---
+++ SlnSharpSystem/src/SharpSystem.Domain/IServices/ISessao.cs
@@ -0,0 +1,16 @@
+﻿using SharpSystem.Domain.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpSystem.Domain.IServices
+{
+    public interface ISessao
+    {
+        void CriarSessaoUsuario(UsuarioDTO usuario);
+        void RemoverSessaoUsuario();
+        UsuarioDTO BuscarSessaoUsuario();
+    }
+}
