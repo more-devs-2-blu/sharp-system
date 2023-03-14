0a912ad14a23b13d45464381bd44d6a1e86f6231
PedroGodri <86849487+pedrogodri@users.noreply.github.com>
2023-03-14 19:08:58 -0300

    Update Login.cs

--- SlnSharpSystem/src/SharpSystem.Domain/Entities/Usuario/Login.cs
+++ SlnSharpSystem/src/SharpSystem.Domain/Entities/Usuario/Login.cs
@@ -9,7 +9,6 @@ namespace SharpSystem.Domain.Entities.Usuario
     public class Login
     {
         public string CFPCNPJ { get; set; }
-<<<<<<< HEAD
         public string Senha { get; set; }
         public string mapTo64()
         {
@@ -23,5 +22,4 @@ namespace SharpSystem.Domain.Entities.Usuario
 =======
         public string Senha { get; set; } 
     }
->>>>>>> 382b642530529f38c9e349e12a7338fff40a45ef
 }
